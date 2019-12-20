8ab76cf482ee7d22b973a8f9c0a2f03ee02fe0e7
NadaSaadi <nadasaadii97@gmail.com>
2019-12-20 13:13:48 +0100

    Application des données sur paris

--- app/src/test/java/com/example/projetandroid/ExampleUnitTest.kt
+++ app/src/test/java/com/example/projetandroid/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
-package com.ismin.opendataapp
+package com.example.projetandroid
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
